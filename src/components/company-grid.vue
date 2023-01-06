5117143466837c40d3ac801489b6e2837cde3bfe
Avishai Dotan <avishaiDotan@gmail.com>
2023-01-06 15:04:20 +0200

    Adding dist

---
+++ src/components/company-grid.vue
@@ -0,0 +1,17 @@
+<template>
+    <section>
+        <section class="work-with-background-grid">
+            <span v-for="n in 8"></span>
+        </section>
+    </section>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
